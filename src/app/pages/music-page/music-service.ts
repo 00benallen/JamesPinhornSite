903a9e22eaa5f4a4431cbdedff00ef2d690bf515
Ben Pinhorn <00ben.allen@gmail.com>
2018-12-20 01:36:59 -0500

    Music page only needs lyric files (and minor tweaks probably) before being done

--- src/app/pages/music-page/music-service.ts
+++ src/app/pages/music-page/music-service.ts
@@ -2,12 +2,24 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Observable } from "rxjs";
 
-import { map } from 'rxjs/operators';
+import { map, share } from 'rxjs/operators';
+
+export interface File {
+    fileName: string
+    displayName: string
+    note: string
+}
+
+export interface Collection {
+    name: string
+    note: string
+    files: File[]
+}
 
 export interface FileList {
     rootFolder: string
     fileExtension: string
-    files: string[]
+    collections: Collection[]
 }
 
 @Injectable()
@@ -16,21 +28,54 @@ export class MusicService {
     constructor(private httpClient: HttpClient) {}
 
     /*
-    JSON File contains list of music file names, its in assets
+    JSON File contains list of music file names, its in assets, this version loads all
     */
-    getMusicFileNames(jsonFile: String): Observable<String[]> {
+    getMusicFileData(jsonFile: String, upToCollectionIndex?: number): Observable<Collection[]> {
 
         return this.httpClient.get<FileList>('assets/' + jsonFile).pipe(
             map((fileList: FileList) => {
-            
-              let newFileList: String[] = []
-              for(let file of fileList.files) {
-                newFileList.push(fileList.rootFolder + file + '.' + fileList.fileExtension)
-              }
-      
-              return newFileList
+
+                let allCollections = fileList.collections
+
+                let collectionsToReturn = []
+                if((upToCollectionIndex || upToCollectionIndex == 0) && upToCollectionIndex < allCollections.length) {
+
+                    for(let i = 0; i <= upToCollectionIndex; i++) {
+                        let collection = allCollections[i]
+
+                        let correctedFiles = this.correctFileNames(collection.files, fileList.rootFolder, fileList.fileExtension)
+
+                        collection.files = correctedFiles
+
+                        collectionsToReturn.push(collection)
+                    }
+
+                    return collectionsToReturn
+
+                } else {
+
+                    for(let collection of fileList.collections) {
+
+                        let correctedFiles = this.correctFileNames(collection.files, fileList.rootFolder, fileList.fileExtension)
+
+                        collection.files = correctedFiles
+
+                    }
+
+                    return fileList.collections
+
+                }
             })
         )
+    }
 
+    private correctFileNames(files: File[], rootFolder: string, fileExtension: string): File[] {
+        let newFileArray: File[] = []
+        for(let file of files) {
+            file.fileName = rootFolder + file.fileName + '.' + fileExtension
+            newFileArray.push(file)
+        }
+
+        return newFileArray
     }
 }
