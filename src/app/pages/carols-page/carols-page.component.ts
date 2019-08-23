2d69b94474abd8afb08d82f2cc53da24491ebee5
Ben Pinhorn <00ben.allen@gmail.com>
2019-08-22 23:46:18 -0400

    V2 is ready to present\!

--- src/app/pages/carols-page/carols-page.component.ts
+++ src/app/pages/carols-page/carols-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carols-page',
   templateUrl: './carols-page.component.html',
-  styleUrls: ['./carols-page.component.css']
+  styleUrls: ['./carols-page.component.scss']
 })
 export class CarolsPageComponent implements OnInit {
 
